33904da331d4b8f4999a948fa41c8163dd95791e
rahathossain3 <rahathossain210@gmail.com>
2022-01-31 05:16:20 +0600

    while loop & for loop using array element

---
+++ array-loop.js
@@ -0,0 +1,15 @@
+// var numbers = [45, 86, 34, 87, 12, 98, 82, 33, 44];
+
+//  // for ( var i = 0; i condition-operator array.length; variable_increase_or_decrease) { anyother_variable_diclear = array[i]; output; }
+// for (var i = 0; i < numbers.length; i++) {
+//     var element = numbers[i]    // i কে  index number হিসাবে বলা হয়েছে
+//     console.log(element);
+// }
+
+
+var items = ['bottle', 'mobile', 'charger', 'watch', 'book', 'paper'];
+
+for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    console.log(item);
+}
